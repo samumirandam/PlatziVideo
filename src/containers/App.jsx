4b56e7b09d9366e3f96bfe04370c190bb44d1d1c
samumirandam <samuelchampi@gmail.com>
2021-02-01 18:22:20 -0500

    Se agregan los estilos del componente Header

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Header from '../Components/Header';
+import '../assets/styles/App.scss';
 
 const App = () => (
   //<div className='app'>
-    <Header />
+  <Header />
   //</div>
 );
 
