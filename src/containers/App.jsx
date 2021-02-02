fbbbd180d1649966bca31b900e059bddbe02326b
samumirandam <samuelchampi@gmail.com>
2021-02-01 19:06:22 -0500

    Se agregan los componentes de Carousel, Categoria y CarouselItem, con sus estilos

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+
 import Header from '../Components/Header';
+import Search from '../Components/Search';
+import Categories from '../Components/Categories';
+import Caruosel from '../Components/Carousel';
+import CaruoselItem from '../Components/CarouselItem';
+
 import '../assets/styles/App.scss';
 
 const App = () => (
-  //<div className='app'>
-  <Header />
-  //</div>
+  <div className='app'>
+    <Header />
+    <Search />
+
+    <Categories>
+      <Caruosel>
+        <CaruoselItem />
+        <CaruoselItem />
+        <CaruoselItem />
+        <CaruoselItem />
+        <CaruoselItem />
+        <CaruoselItem />
+      </Caruosel>
+    </Categories>
+  </div>
 );
 
 export default App;
